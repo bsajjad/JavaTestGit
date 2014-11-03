e2f4ac5fe6e4c386baa835fa511bcb09dc9ed246
Bushra <bsajjad@andrew.cmu.edu>
2014-11-03 18:01:14 +1030

    Initial commit

---
+++ src/testgitproject/TestGitProject.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testgitproject;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Bushra Sajjad
+ */
+public class TestGitProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a, b, c;
+        Random rand=new Random();
+        a= rand.nextInt();
+        b= rand.nextInt();
+        
+        System.out.println("Sum is: " + a+b);
+        
+        
+        
+        
+    }
+    
+}
